6d4c4a7f1286ed75fd5e27aab2f7c7c92db76f29
Tijn Liefrink <tijnliefrink@outlook.com>
2024-01-15 12:51:14 +0100

    GroepsSelectie gemaakt en styles aanpassingen

---
+++ Pages/GroepsSelectie.php
@@ -0,0 +1,10 @@
+<?php
+include '../includes/header.php';
+
+?>
+
+<body>
+    <h1 class="Titel">Groeps selectie</h1>
+    <h3>Voer groeps naam in</h3>
+    <input type="text" class="textbox">
+</body>
