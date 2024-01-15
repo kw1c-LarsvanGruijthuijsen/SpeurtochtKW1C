144ad45fc7f667fc9dea75e9c42c07b2e9fb8671
lars <lars.vangruijthuijsen@edu-kw1c.nl>
2024-01-15 22:34:01 +0100

    group selectie update

--- Pages/GroepsSelectie.php
+++ Pages/GroepsSelectie.php
@@ -1,16 +1,30 @@
 <?php
-include '../includes/header.php';
-
+include '../Includes/header.php';
 ?>
-
 <body>
-    <h1 class="Titel">Groeps selectie</h1>
-    <h3>Voer groeps naam in</h3>
-    <input type="text" class="textbox">
-    <h4>Namen leerlingen</h4>
-    <input type="text" class="textbox-klein">
-    <input type="text" class="textbox-klein">
-    <input type="text" class="textbox-klein"><br>
-    <button class="extra-naam-knop">+ </button>
-    
+
+<div class="container group-container">
+    <div class="row justify-content-center">
+        <div class="col-md-auto">
+            <div class="form-container text-center">
+                <h2>Groep selectie</h2>
+                <p>Voer groeps naam in</p>
+                <input type="text" class="form-control mb-3" placeholder="Groepsnaam">
+                <p>Namen leerlingen</p>
+                <div class="mb-3">
+                    <input type="text" class="form-control mb-2" placeholder="Leerling 1">
+                    <input type="text" class="form-control mb-2" placeholder="Leerling 2">
+                    <input type="text" class="form-control mb-2" placeholder="Leerling 3">
+                </div>
+                <div class="d-flex align-items-center justify-content-center mb-3">
+                    <div class="circle-container">
+                        <i class='bx bx-plus'></i>
+                    </div>
+                </div>
+                <button type="button" class="btn group-next">Volgende</button>
+            </div>
+        </div>
+    </div>
+</div>
+
 </body>
