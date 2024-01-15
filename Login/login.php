898328ef7d432fb80a11b98cfc879f429aa77eca
Dylan Verploegen <dy.di.ve@outlook.com>
2024-01-15 13:00:46 +0100

    woop wopp

--- Login/login.php
+++ Login/login.php
@@ -7,6 +7,7 @@ if (isset($_SESSION['username'])) {
 }
 
 include '../Includes/head.php';
+include '../Includes/header.php';
 ?>
 
 <!DOCTYPE html>
@@ -16,10 +17,7 @@ include '../Includes/head.php';
     <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-<div class="header">
-    <img class="img" src="../Images/Logo_KW1C.svg" alt="Description of the image" width="100" height="75">
-    <h2 class="header-txt">Login</h2>
-</div>
+
 
 <form method="post" action="login.php">
     <!-- Display errors here -->
@@ -31,15 +29,14 @@ include '../Includes/head.php';
         </div>
     <?php endif ?>
 
-    <div class="input-group">
-        <label>Gebruikersnaam</label>
-        <input type="text" name="username" required>
-    </div>
-
-    <div class="input-group">
-        <label>Wachtwoord</label>
-        <input type="password" name="password" required>
-    </div>
+    <div  class="input-group" class="mb-3" >
+  <label for="exampleFormControlInput1" class="form-label">Gebruikersnaam</label>
+  <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="gebruikersnaam" name="username" required>
+</div>
+<div  class="input-group" class="mb-3" >
+<label for="exampleFormControlInput1" class="form-label">Wachtwoord</label>
+  <input type="password" class="form-control" id="exampleFormControlInput1" placeholder="wachtwoord" name="password" required>
+</div>
 
     <div class="input-group">
         <button type="submit" class="btn" name="login_user">Login</button>
